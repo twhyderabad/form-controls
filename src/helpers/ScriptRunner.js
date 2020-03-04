50c7cd45109443b02f75b24709b7ea8dbbf06632
Alekhya <alekhyayalla23@gmail.com>
2020-03-04 12:09:32 +0530

    Alekhya | FBE-19 | Pass httpinterceptor to script for making api calls

--- src/helpers/ScriptRunner.js
+++ src/helpers/ScriptRunner.js
@@ -1,15 +1,18 @@
 import FormContext from './FormContext';
+import { httpInterceptor } from '../helpers/httpInterceptor';
 
 export default class ScriptRunner {
 
   constructor(formRecords, patient) {
     this.formContext = new FormContext(formRecords, patient);
+    this.interceptor = httpInterceptor;
   }
 
   execute(eventJs) {
     const formContext = this.formContext;
-    if (eventJs) {
-      const executiveJs = `(${eventJs})(formContext)`;
+    const interceptor = this.interceptor;
+    if (eventJs && interceptor) {
+      const executiveJs = `(${eventJs})(formContext,interceptor)`;
       /* eslint-disable */
       eval(executiveJs);
     }
