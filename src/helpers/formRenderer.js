fe6bfc4d03dcca2541b8f1707d363b63fb3044e8
sowmika <sowmika148@gmail.com>
2020-03-30 13:48:38 +0530

    Sowmika, Alekhya | FBE-70 | Add window methods to run form event and get observations

--- src/helpers/formRenderer.js
+++ src/helpers/formRenderer.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ControlRecordTreeBuilder from 'src/helpers/ControlRecordTreeBuilder';
 import ScriptRunner from './ScriptRunner';
+import ObservationMapper from 'src/helpers/ObservationMapper';
 
 window.renderWithControls =
   function renderWithControls(formDetails, observations, nodeId,
@@ -21,8 +22,7 @@ window.unMountForm = (container) => {
 window.getRecordTree = (formDef, observations) =>
     new ControlRecordTreeBuilder().build(formDef, observations);
 
-window.runScript = (form) => {
-  const component = form.component;
-  return new ScriptRunner(component.state.data, component.props.patient)
-      .execute(form.events.onFormSave);
-};
+window.runEventScript = (formData, eventScript, patient) => new ScriptRunner(formData, patient)
+      .execute(eventScript);
+
+window.getObservations = (records) => (new ObservationMapper()).from(records);
